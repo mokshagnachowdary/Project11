3045c34b727a50c182bf4e0cbede163d9d7b15c2
mokshagnachowdary <mokshagnachowdharyy@gmail.com>
2026-02-23 11:50:56 +0530

    first commit

---
+++ src/MarksContext.jsx
@@ -0,0 +1,14 @@
+import React, {useContext,useState, createContext} from 'react';
+export const MarksContext=createContext();
+export default function MarksProvider({children}) {
+    const [marks, setMarks]=useState([]);
+    function addMarks(student){
+        setMarks([...marks, student]);
+    }
+  return (
+    <MarksContext.Provider value={{marks, addMarks}}>
+        {children}
+    </MarksContext.Provider>
+  )
+}
+
